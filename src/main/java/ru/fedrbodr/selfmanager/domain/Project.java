7a2627ca069ae5bf7819389975ae4ce4ea9c041d
Dmitry Fedorenko <fedrbodr@gmail.com>
2019-08-30 20:50:16 +0300

    Initial commit

---
+++ src/main/java/ru/fedrbodr/selfmanager/domain/Project.java
@@ -0,0 +1,4 @@
+package ru.fedrbodr.selfmanager.domain;
+
+public class Project {
+}
---
+++ src/test/java/ru/fedrbodr/selfmanager/SelfManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.fedrbodr.selfmanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SelfManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
